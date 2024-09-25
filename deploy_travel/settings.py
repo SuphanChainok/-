09c4a51d8c2a071a1515efb7098ff3a1d03ac83e
Suphan <148407664+SuphanChainok@users.noreply.github.com>
2024-09-25 17:05:58 +0700

    อีกนิด

--- deploy_travel/settings.py
+++ deploy_travel/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-on@2hubwg_o5n*f&--&s!_+0ucc16q+uhi#m_%xbwn=jrt$--v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
